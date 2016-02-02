7cdb24f4f95b689b4208411b55406f33daedb3d5
Boleslav Prokofiev <boleslav.prokofiev@gmail.com>
2016-02-02 19:07:00 +0300

    further jenkins integration

--- src/test/java/sample/RunCukesTest.java
+++ src/test/java/sample/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format={"pretty","html:reports/test-report"},tags=" @smokeTest")
+@CucumberOptions(strict = true,format = {"pretty", "html:target/cukes", "json:target/cukes/report.json", "junit:target/cukes/junit.xml"},tags=" @smokeTest")
 public class RunCukesTest {
 }
